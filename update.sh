e71b5e2964aac5dbb002ba4d49e42408230b77e9
Billy 'bvierra' Vierra <bvierra@cpan.org>
2014-10-19 13:17:48 +0000

    Added update file

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git update
+git submodule update --init
+
+mkdir ~/.vim
+
+ln -sf ~/.etc/Dotfiles/vim-files/autoload ~/.vim/autoload
+ln -sf ~/.etc/Dotfiles/vim-files/bundle ~/.vim/bundle
+ln -sf ~/.etc/Dotfiles/vim-files/vimrc ~/.vimrc
+
+ln -sf ~/.etc/Dotfiles/misc/.ctags ~/.ctags
