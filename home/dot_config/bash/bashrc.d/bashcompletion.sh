05893840e93359735d18ed37919bcce98083693e
Billy Vierra <bvierra@gmail.com>
2024-11-18 21:13:17 -0800

    Fixes
    Signed-off-by: Billy Vierra <bvierra@gmail.com>

---
+++ home/dot_config/bash/bashrc.d/bashcompletion.sh
@@ -0,0 +1,13 @@
+# shellcheck shell=bash
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+# This code is also in /etc/bash.bashrc but is commented out by default
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+# vim: set filetype=bash:
